17313823a5c8ff061e96734f879bfaf44b398088
marocks31 <mollyarisley@gmail.com>
2022-08-30 18:09:45 -0500

    style changes to craft showpage

--- src/views/CraftsShow.vue
+++ src/views/CraftsShow.vue
@@ -26,11 +26,22 @@ export default {
   <div class="crafts-show">
     <h1>A Closer look..</h1>
     <h2>{{ craft.name }}</h2>
-    <img v-bind:src="craft.image" v-bind:alt="craft.name" />
-    <p>{{ craft.description }}</p>
+    <img id="craft-image" v-bind:src="craft.image" v-bind:alt="craft.name" />
+    <h2>Description:</h2>
+    <h4>{{ craft.description }}</h4>
+    <h2>Difficulty:</h2>
+    <h4>{{ craft.difficulty }}</h4>
+    <h2>Materials:</h2>
+    <h4>{{ craft.materials }}</h4>
+
     <router-link v-bind:to="`/crafts/${craft.id}/edit`">Edit</router-link>
     <router-link v-bind:to="`/comments/${craft.id}/edit`">Comment</router-link>
     <button v-on:click="destroyCraft(craft)">Delete</button>
     <router-link to="/crafts">Back to all</router-link>
   </div>
 </template>
+<style>
+#craft-image {
+  height: 50vh;
+}
+</style>
